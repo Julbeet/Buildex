dc2ca062843f7a556452478dcd769df713c8030f
JulBeet <julbeet@gmail.com>
2018-04-10 15:51:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+	$('.slider').slick({
+		prevArrow:"<div class='prevArrow'></div>",
+      	nextArrow:"<div class='nextArrow'></div>",
+		autoplay: true,
+		autoplaySpeed: 4000,
+		infinite: true,
+	
+	});
+
+
+
+
+})(jQuery)
